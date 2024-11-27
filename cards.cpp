9200ba5a09a0dc843bcd13b63308329d3dbc3c84
Gage Robison <grobison@student.rccd.edu>
2024-11-27 14:47:43 -0800

    Added cards.cpp

---
+++ cards.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+
+enum Rank
+{
+	TWO,
+	THREE,
+	FOUR,
+	FIVE,
+	SIX,
+	SEVEN,
+	EIGHT,
+	NINE,
+	TEN,
+	JACK,
+	QUEEN,
+	KING,
+	ACE
+};
+
+enum Suit
+{
+	CLUBS,
+	SPADES,
+	HEARTS,
+	DIAMONDS
+};
+
+class Card
+{
+private:
+	Rank rank;
+	Suit suit;
+public:
+	Card(Rank r, Suit s) : rank(r), suit(s) {}
+
+	string getRank() const
+	{
+		switch (rank)
+		{
+		case TWO: return "2";
+		case THREE: return "3";
+		case FOUR: return "4";
+		case FIVE: return "5";
+		case SIX: return "6";
+		case SEVEN: return "7";
+		case EIGHT: return "8";
+		case NINE: return "9";
+		case TEN: return "10";
+		case JACK: return "Jack";
+		case QUEEN: return "Queen";
+		case KING: return "King";
+		case ACE: return "Ace";
+		}
+
+		return "";
+	}
+
+	string getSuit() const {
+		switch (suit)
+		{
+		case HEARTS: return "Hearts";
+		case DIAMONDS: return "Diamonds";
+		case CLUBS: return "Clubs";
+		case SPADES: return "Spades";
+		}
+
+		return "";
+	}
+
+	void displayCard() const
+	{
+		cout << getRank() << " of " << getSuit() << endl;
+	}
+};
+
+int cards()
+{
+	Card card1(TEN, SPADES);
+
+	card1.displayCard();
+
+	return 0;
+}
